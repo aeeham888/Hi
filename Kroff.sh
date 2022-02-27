d80dbc0b7b209879fb23a3c0b09e33d1b96465d2
aeeham88 <migxwapsh@gmail.com>
2022-02-27 13:25:15 +0000

    Create Kroff.sh

---
+++ Kroff.sh
@@ -0,0 +1,26 @@
+MOSTAFA="com.pubg.krmobile"
+
+DUMP() {
+
+  pm dump $1 | grep $2 | tr ' ' '\n' | grep $1 | sed s/$2// | tr -d '\n';
+
+};
+
+lib=`ls -mR $(DUMP $MOSTAFA legacyNativeLibraryDir=) | grep : | tr -d : | grep /arm | grep -v dora`
+
+am force-stop $MOSTAFA
+
+pkill $MOSTAFA
+
+su -c iptables --flush
+
+iptables --flush
+
+iptables -F
+
+ip6tables -F
+
+chmod -R 755 $lib/* &> /dev/null
+
+pm install -r /data/app/$MOSTAFA*/base.apk
+
